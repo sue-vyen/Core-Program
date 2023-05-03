777aa980865da32fe52bd3f57705b1422a89c4dd
Sue-Vyen Loke <sloke@u81z04s02.42kl.edu.my>
2023-05-03 12:00:12 +0800

    tab?

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,26 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sloke <sloke@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/02 12:48:57 by sloke             #+#    #+#             */
-/*   Updated: 2023/05/03 11:59:22 by sloke            ###   ########.fr       */
+/*   Created: 2023/05/03 11:38:00 by sloke             #+#    #+#             */
+/*   Updated: 2023/05/03 11:38:04 by sloke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdio.h>
-# include <unistd.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *c);
-
-#endif
+int	ft_isprint(int c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	else
+		return (0);
+}
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,26 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sloke <sloke@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/02 12:48:57 by sloke             #+#    #+#             */
-/*   Updated: 2023/05/03 11:59:22 by sloke            ###   ########.fr       */
+/*   Created: 2023/05/03 11:40:46 by sloke             #+#    #+#             */
+/*   Updated: 2023/05/03 11:57:57 by sloke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdio.h>
-# include <unistd.h>
+size_t	ft_strlen(const char *c)
+{
+	unsigned int	pos;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *c);
+	pos = 1;
+	while (c[pos])
+	{
+		pos++;
+	}
+	return (pos);
+}
 
-#endif
+/*int main()
+{
+    const char  *v = "no you";
+    printf("the string length is: %zu", ft_strlen(v));
+    return (0);
+}*/
