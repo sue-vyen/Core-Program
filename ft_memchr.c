bbf77899f35af1a21381afc76ca651c83564f166
Sue-Vyen Loke <sloke@u81z06s01.42kl.edu.my>
2023-05-14 12:40:19 +0800

    omega-3

---
+++ ft_memchr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sloke <sloke@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/14 11:43:05 by sloke             #+#    #+#             */
+/*   Updated: 2023/05/14 12:39:50 by sloke            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			pos;
+	unsigned char	*new_s;
+
+	pos = 0;
+	new_s = (unsigned char *)s;
+	while (pos < n)
+	{
+		if (new_s[pos] == c)
+			return ((void *)s + pos);
+		pos++;
+	}
+	return (0);
+}
+
+// int	main(void)
+// {
+// 	char	str[] = "no you";
+
+// 	printf("%s", ft_memchr(str, 'l', 6));
+// 	return (0);
+// }
+
+/* finding the first occurence of a specific char
+if it isn't found with the n bytes inputed,
+return (NULL) / return (0);
+if it is, the position of the first occurence should return;
+slayyed kidney 
+void pointer allows the f(x) to return a pointer to any type of data,
+being called can be easier. */
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sloke <sloke@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 13:31:02 by sloke             #+#    #+#             */
-/*   Updated: 2023/05/14 11:34:35 by sloke            ###   ########.fr       */
+/*   Updated: 2023/05/14 12:20:05 by sloke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
