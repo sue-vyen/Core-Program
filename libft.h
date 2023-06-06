11e32b05df4043341239a9762381ef25b6e651e2
Sue-Vyen Loke <sloke@u81z06s01.42kl.edu.my>
2023-06-06 15:56:31 +0800

    squishy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sloke <sloke@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/13 13:31:02 by sloke             #+#    #+#             */
-/*   Updated: 2023/06/06 15:38:54 by sloke            ###   ########.fr       */
+/*   Updated: 2023/06/06 15:51:15 by sloke            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,10 @@ typedef struct s_list
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
+
 void	ft_lstadd_front(t_list **lst, t_list *new);
+
 int		ft_lstsize(t_list *lst);
 
 #endif
